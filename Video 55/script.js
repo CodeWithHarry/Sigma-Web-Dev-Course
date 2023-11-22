ebb1b8fefa4c97110dc06365cc62d92fadd8b8d4
Haris Ali Khan <iitianharis@gmail.com>
2023-11-22 17:41:56 +0530

    Video 55 Completed

---
+++ Video 55/script.js
@@ -0,0 +1,42 @@
+console.log("Hey this is tutorial 55");
+
+var a = 5;
+// a = a + 1
+let b = 6;
+let c = "Harry";
+let _a = "Shubham";
+// var 55a = "Rohan"; // Not Allowed
+
+// console.log(a +  b + 8);
+// console.log(typeof a, typeof b, typeof c);
+{
+    // let a = 66;
+    console.log(a)
+}
+console.log(a)
+// const a1 = 6;
+// a1 = a1 + 1; // Not Allowed because a1 is constant
+
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    "name": "Harry",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
