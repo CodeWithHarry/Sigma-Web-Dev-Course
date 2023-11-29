93d37f0d77c84fcac2b7677aeb35b8d5b5c556dc
Haris Ali Khan <iitianharis@gmail.com>
2023-11-29 18:49:05 +0530

    Video 63 Completed

---
+++ Viideo 63/loops.js
@@ -0,0 +1,28 @@
+let a = [1, 93, 5, 6, 88]
+
+
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element)
+    
+// }
+
+// a.forEach((value, index, arr)=>{
+//     console.log(value, index, arr)
+// })
+
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key]; 
+//         console.log(key, element)
+//     }
+// }
+
+for (const value of a) {
+    console.log(value)
+}
