03f59ed118bf70dc505108829ee32ee6b79b4385
Haris Ali Khan <iitianharis@gmail.com>
2023-11-29 18:48:11 +0530

    Video 62 Completed

---
+++ Video 62/index.js
@@ -0,0 +1,23 @@
+/* Create a business name generator by combining list of adjectives and shop name and another word
+
+
+Adjectives:
+Crazy 
+Amazing
+Fire 
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another Word:
+Bros
+Limited
+Hub
+
+
+
+*/
+
+
