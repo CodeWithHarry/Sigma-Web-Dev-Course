d3e6bbf786c45474cc3576182c0e97322f80d66b
Haris Ali Khan <iitianharis@gmail.com>
2023-12-15 13:18:38 +0530

    Video 73 Completed

---
+++ Video 73/script.js
@@ -0,0 +1,6 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail){
+    // Finish this function
+}
+
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
