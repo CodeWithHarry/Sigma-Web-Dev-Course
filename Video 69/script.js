c583f59639a978553c96d5b5b29e0bc0f6bd6f66
Haris Ali Khan <iitianharis@gmail.com>
2023-12-15 13:18:07 +0530

    Video 69 Completed

---
+++ Video 69/script.js
@@ -0,0 +1,18 @@
+let a = 7
+
+function factorial(number){
+   let arr = Array.from(Array(number+1).keys())
+   let c = arr.slice(1,).reduce((a, b)=> a*b )
+   return c
+}
+
+
+function facFor(number){
+    let fac = 1;
+    for (let index = 1; index <= number; index++) {
+        fac = fac * index
+    }
+    return fac
+}
+console.log(factorial(a))
+console.log(facFor(a))
