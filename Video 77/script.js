442b6486e20c63ed471b1017d1d470270376911e
Haris Ali Khan <iitianharis@gmail.com>
2023-12-16 18:49:17 +0530

    Video 77 Completed

---
+++ Video 77/script.js
@@ -0,0 +1,29 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail){
+    // Finish this function
+    let viewStr
+    if (views<1000){
+        viewStr = views; 
+    }
+    else if(views>1000000){
+        viewStr = views/1000000 + "M"; 
+    }
+    else{
+        viewStr = views/1000 + "K"; 
+    }
+    let html = `<div class="card">
+    <div class="image">
+        <img src="${thumbnail}"
+            alt="">
+        <div class="capsule">${duration}</div>
+    </div>
+    <div class="text">
+        <h1>${title}</h1>
+        <p>${cName} . ${viewStr} views . ${monthsOld} months ago</p>
+    </div> 
+</div>`
+
+document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
+}
+
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
