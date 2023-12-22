5562a5c161e8b78de41dda95891df53fef2919d1
satyakideshpande <satyaki747@gmail.com>
2023-12-22 23:18:45 +0530

    Fixed File Name For Video 63

---
+++ Video 63/index.js
@@ -0,0 +1,21 @@
+let arr = [1, 2, 4, 5, 7]
+//  Index  0, 1, 2, 3, 4
+
+arr[0] = 5666;
+// console.log(arr, typeof arr);
+// console.log(arr.length)
+
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[4])
+
+console.log(arr.toString())
+console.log(arr.join(" and "))
+
+
+ 
+// let numbers = [1, 2, 3, 4, 5] 
+// numbers.splice(1, 2)    
+// numbers.splice(1, 3)  
+// numbers.splice(1, 3, 222, 333) 
+// (4) [1, 222, 333, 5]
