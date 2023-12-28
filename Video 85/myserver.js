0b1032973412e5f2a5fe612d96f2afaa6c70fd1c
Haris Ali Khan <iitianharis@gmail.com>
2023-12-28 21:05:55 +0530

    Video 85 Completed

---
+++ Video 85/myserver.js
@@ -0,0 +1,15 @@
+// Further Reading: https://nodejs.org/en/learn/getting-started/introduction-to-nodejs
+const http = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
