3aab9e241bac2abbd2a3d4b19e0569060bdb0a89
Haris Ali Khan <iitianharis@gmail.com>
2024-01-20 05:11:11 +0530

    Video 97 Completed

---
+++ Video 97/main.js
@@ -0,0 +1,13 @@
+// Generate a dummy data in this format in a collection called employees in a db called company
+
+// {
+//     name: "Harry",
+//     salary: 45000000,
+//     language: "Python",
+//     city: "New York",
+//     isManager: true
+// }
+
+// Generate 10 such records when a button called generate data is clicked!
+// Create an Express app with mongoose to acheive it
+// Everytime the button is clicked, you should clear the collection 
