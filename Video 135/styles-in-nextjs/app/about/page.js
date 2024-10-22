7e2418d47d364b0d3f73d408d67f26ff150ee478
Haris Ali Khan <iitianharis@gmail.com>
2024-10-23 02:07:55 +0530

    Video 135 Completed

---
+++ Video 135/styles-in-nextjs/app/about/page.js
@@ -0,0 +1,28 @@
+"use client"
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+
+            <div className='container'>
+
+                <h1>This is about me</h1>
+                <p>Hey I am a good boy</p>
+
+                <style jsx>{`
+            .container{
+                background-color: red;
+                color: green;
+                }
+                `}
+                </style>
+            </div>
+            <div className="container">
+                Hey I am good
+            </div>
+        </div>
+    )
+}
+
+export default About
---
+++ Video 135/styles-in-nextjs/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../styles/home.module.css"
+
+export default function Home() {
+  return (
+    <div className={styles.red}>
+      Hey
+    </div>
+  );
+}
